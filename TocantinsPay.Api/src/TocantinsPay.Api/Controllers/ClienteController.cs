a3012fe87240844f19692c4c907afc42c5777f34
Allan Dutra <allandutrafeitosa14@gmail.com>
2025-12-12 19:44:51 -0300

    feat: cliente controller

---
+++ TocantinsPay.Api/src/TocantinsPay.Api/Controllers/ClienteController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TocantinsPay.Core.Interfaces.Applications;
+using TocantinsPay.Core.Models.InputModels;
+
+namespace TocantinsPay.Api.Controllers
+{
+    public class ClienteController(ICadastrarClienteApplication cadastrarClienteApplication) : MainController
+    {
+        private readonly ICadastrarClienteApplication _cadastrarClienteApplication = cadastrarClienteApplication;
+
+        [HttpPost]
+        public async Task<IActionResult> CadastrarAsync([FromBody] ClienteInputModel inputModel)
+        {
+            var id = await _cadastrarClienteApplication.CadastrarAsync(inputModel);
+
+            return Ok(id);
+        }
+    }
+}
---
+++ TocantinsPay.Api/src/TocantinsPay.Api/Controllers/MainController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TocantinsPay.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class MainController : ControllerBase
+    {
+
+    }
+}
